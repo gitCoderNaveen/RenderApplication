4ffd5b967a02ca1d997d9c1b1db532ba4af50d12
JAVAVEEN <naveenbsc.mca1518@gmail.com>
2024-08-13 15:02:57 +0530

    final update database working

--- backend/index.js
+++ backend/index.js
@@ -8,11 +8,16 @@ const app = express()
 
 app.use(cors())
 
-mongoose.connect('mongodb+srv://naveenbscmca1518:zlTWvCrHrooGlLov@newcluster.8zvx9.mongodb.net/ProductDirectory?retryWrites=true&w=majority&appName=NewCluster')
+// const uri = 'mongodb+srv://naveenbscmca1518:zlTWvCrHrooGlLov@newcluster.8zvx9.mongodb.net/ProductDirectory?retryWrites=true&w=majority&appName=NewCluster';
+mongoose.connect('mongodb+srv://naveenbscmca1518:user123@mernapi.exvm5.mongodb.net/ProductDirectory?retryWrites=true&w=majority&appName=MernApi')
 
 mongoose.connection.on('connected', ()=>{
     console.log('MongoDB successfully connectd')
 })
+
+// mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
+//   .then(() => console.log('MongoDB connected'))
+//   .catch(err => console.error('MongoDB connection error:', err));
 
 app.use('/datas',datas)
 
